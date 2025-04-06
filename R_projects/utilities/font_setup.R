63b5b3bdb222f5c7626ab10c2ec2a7aa89a3294b
gordon-goodwin <155502423+gordon-goodwin@users.noreply.github.com>
2025-04-06 17:24:38 -0600

    added utility scripts
    script for utility functions and set-up (fonts and themes)

---
+++ R_projects/utilities/font_setup.R
@@ -0,0 +1,42 @@
+
+
+# Font Utility Functions & Set-Up -----------------------------------------
+
+# 2 main utility functions for fonts
+## a) setup_fonts: Fonts Added + Settings Config
+## b) get_font_fams: Ties specified font family names to list items/names
+
+library(showtext) 
+
+# Font Config Function
+## Adds Fonts from Google Repo & Specifies FAmily Name & Settings
+setup_fonts <- function() {
+  
+  # Add Google Fonts
+  font_add_google("Oswald", family = "oswald", db_cache = FALSE)
+  font_add_google("Roboto Slab", family = "roboto_slab", db_cache = FALSE)
+  font_add_google("Roboto Condensed", family = "roboto_condensed", db_cache = FALSE)
+  font_add_google("Roboto Serif", family = "roboto_serif", db_cache = FALSE)
+  font_add_google("Roboto", family = "roboto", db_cache = FALSE)
+  font_add_google("Noto Sans", family = "noto_sans", db_cache = FALSE)
+  font_add_google("Open Sans", family = "open_sans", db_cache = FALSE)
+  font_add_google("Lato", family = "lato", db_cache = FALSE)
+  
+  # Enable showtext
+  showtext_auto(enable = TRUE)
+  showtext_opts(dpi = 320, regular.wt = 300, bold.wt = 800)
+}
+
+
+
+# Font Accessory Function
+## Facilitates for usability in plotting
+### -> ties each font family name to a list object/item
+get_font_families <- function() {
+  list(
+    title = "roboto_condensed",
+    subtitle = "roboto",
+    text = "roboto",
+    caption = "noto_sans"
+  )
+}
---
+++ R_projects/utilities/theme_setup.R
@@ -0,0 +1,81 @@
+
+
+# Theme Utility Functions & Set-Up ----------------------------------------
+
+
+# Default & Dynamic/Customizable Color Specifications
+## Uses list to tie core plot elements to specified default colors
+### -> adds "palette" list item for dynamic palette input as well
+get_theme_colors <- function(palette = NULL) {
+  list(
+    background = "#f5f5f2",
+    title      = "gray20",
+    subtitle   = "gray30",
+    text       = "gray30",
+    caption    = "gray40",
+    palette    = palette  
+  )
+}
+
+
+
+
+# Base Theme
+## -> Core elements that are common across iterations/contexts
+### -> Args for Colors and Fonts List Item References
+create_base_theme <- function(colors = get_theme_colors()) {
+  
+  theme_minimal(base_size = 12, base_family = fonts$text) +
+    theme(
+      
+      plot.title.position   = "plot",
+      plot.caption.position = "plot",
+      legend.position       = "top",
+      
+      # Background elements filled using specified color palette for each element
+      plot.background  = element_rect(fill = colors$background, color = colors$background),
+      panel.background = element_rect(fill = colors$background, color = colors$background),
+      
+      # Panel Grid
+      panel.grid.major = element_line(color = "grey88"),
+      panel.grid.minor = element_line(color = "grey88"),
+      
+      # Set Plot margins
+      # plot.margin = margin(t = 10, r = 20, b = 10, l = 20),
+      
+      # Fixed axis text & title elements tied to specified colors and fonts
+      axis.text  = element_text(
+        family = fonts$text,
+        size   = rel(0.79),
+        color  = colors$text
+      ),
+      axis.title = element_text(
+        family = fonts$subtitle,
+        size   = rel(0.93),
+        face   = "bold",
+        color  = colors$text
+      ),
+      plot.title = element_text(
+        family = fonts$title,
+        face = "bold",
+        color = colors$title
+      ),
+      plot.subtitle = element_text(
+        family = fonts$title,
+        color = colors$subtitle
+      )
+      
+      
+    )
+}
+
+
+
+
+# Extend Base Theme - allows for specifying non-core theme adjustments
+extend_base_theme <- function(base_theme, extended_elements) {
+  
+  # Returns Base Theme & Adjustments/Additions
+  base_theme + extended_elements
+}
+
